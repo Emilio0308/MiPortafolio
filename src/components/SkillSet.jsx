04ece4f982614a287c835c4c6c33e54d196b4f3e
emilio rivas <emiliorivasruiz@gmail.com>
2023-05-11 01:56:56 -0500

    skill bg

--- src/components/SkillSet.jsx
+++ src/components/SkillSet.jsx
@@ -3,12 +3,15 @@ import SkillCard from "./SkillCard";
 
 const SkillSet = () => {
   return (
-    <section id="skills" className="w-full max-w-[1200px] mx-auto flex flex-col gap-8 p-3 pt-[90px]">
+    <section
+      id="skills"
+      className="w-full max-w-[1200px] mx-auto flex flex-col gap-8 p-3 pt-[60px] mt-[80px] relative"
+    >
       <h2 className="text-3xl after:bg-cyan-500 font-semibold tracking-[8px]">
         <span className="text-cyan-500 text-4xl">#</span>My Skills
       </h2>
 
-      <div className="w-full grid auto-rows-auto grid-cols-[repeat(auto-fit,_minmax(260px,_1fr))] gap-5">
+      <div className="w-full grid auto-rows-auto grid-cols-[repeat(auto-fit,_minmax(260px,_1fr))] gap-5 mt-[40px] mb-[80px] z-10">
         <SkillCard
           type={"Soft"}
           skills={[
@@ -35,18 +38,8 @@ const SkillSet = () => {
           skills={["Nodejs", "MongoDB", "Phyton", "SQL"]}
         />
       </div>
-      {/* <article className="grid-cols-2">
-        <div className="h-[300px] overflow-hidden">
-          <img
-            className="h-full bottom-0"
-            src="/imagenes/cel2.png"
-            alt=""
-          />
-        </div>
-        <div>
-          
-        </div>
-      </article> */}
+
+      <div className="bg-[url(/imagenes/phone.png)] w-full right-0 left-0 top-0 bottom-0 opacity-5 absolute z-0"></div>
     </section>
   );
 };
